818d281a51ef3fbdca73efcc0bd408b2529c0c23
Stephan Wendt <stephanwendt@freenet.de>
2016-09-10 00:55:02 +0200

    Configure deployment on anynines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,25 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+#	  :address => "mail.anynines.com",
+#	  :port => 587,
+#	  :user_name => ENV['EMAIL_USERNAME'],
+#	  :password => ENV['EMAIL_PASSWORD'],
+#	  :authentication => :login,
+#	  :enable_starttls_auto => true
+	  :address              =>  'smtp.sendgrid.net',
+	  :port                 =>  '587',
+	  :authentication       =>  :plain,
+	  :user_name            =>  ENV['SENDGRID_USERNAME'],
+	  :password             =>  ENV['SENDGRID_PASSWORD'],
+	  :domain               =>  'heroku.com',
+	  :enable_starttls_auto  =>  true
+  }
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-#require 'carrierwave'
-#require 'a9s_swift'
-#
-#BUCKET_NAME = "images"
-#
-#if Rails.env.production?
-#  Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
-#end
+require 'carrierwave'
+require 'a9s_swift'
+
+BUCKET_NAME = "images"
+
+if Rails.env.production?
+  Anynines::Swift::Utility.configure_carrierwave(BUCKET_NAME,{fog_public: false}, "openstack")
+end
 
 #CarrierWave.configure do |config|
 #	config.fog_credentials = {
